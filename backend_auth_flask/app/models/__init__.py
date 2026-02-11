c2dc599431ce42d805cbd7e43e46da853cddc5b6
sar <frederic.sar@etu.umontpellier.fr>
2026-02-11 11:18:01 +0100

    sauvegarde avant basculement sur main

--- backend_auth_flask/app/models/__init__.py
+++
@@ -1,5 +0,0 @@
-from .user import User
-from .token import Token
-from .reset import Reset
-
-__all__ = ['User', 'Token', 'Reset']
--- backend_auth_flask/app/models/token.py
+++
@@ -1,9 +0,0 @@
-from .. import db
-import datetime
-
-class Token(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    token = db.Column(db.String(255), nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    created_at = db.Column(db.DateTime, default=datetime.datetime.utcnow)
-    expired_at = db.Column(db.DateTime, nullable=False)
