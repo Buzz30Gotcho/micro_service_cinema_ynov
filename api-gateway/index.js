c9f04292cea70de327de42dc9ce219ff95736502
sar <frederic.sar@etu.umontpellier.fr>
2026-02-27 20:56:12 +0100

    Fix API gateway /movies routing

--- api-gateway/index.js
+++ api-gateway/index.js
@@ -14,7 +14,7 @@ app.use(cors({
         'http://localhost:5173'
     ],
     credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
     allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
